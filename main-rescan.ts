f5dff7a4e619dd7d9da143a1fc566d013176c074
cal2195 <calmart101@gmail.com>
2020-02-27 12:48:07 +0000

    Better callback

--- main-rescan.ts
+++ main-rescan.ts
@@ -33,56 +33,14 @@ export function watchInputFolder(
     }
   });
 
-  let data = {}; //used to correlate two events
-
   const watchCallback = (event) => {
-      let mask = event.mask;
-      let type = mask & Inotify.IN_ISDIR ? 'directory ' : 'file ';
-      if (event.name) {
-          type += ' ' + event.name + ' ';
-      } else {
-          type += ' ';
-      }
-      // the purpose of this hell of 'if' statements is only illustrative.
-
-      if (mask & Inotify.IN_ACCESS) {
-          console.log(type + 'was accessed ');
-      } else if (mask & Inotify.IN_MODIFY) {
-          console.log(type + 'was modified ');
-      } else if (mask & Inotify.IN_OPEN) {
-          console.log(type + 'was opened ');
-      } else if (mask & Inotify.IN_CLOSE_NOWRITE) {
-          console.log(type + ' opened for reading was closed ');
-      } else if (mask & Inotify.IN_CLOSE_WRITE) {
-          console.log(type + ' opened for writing was closed ');
-      } else if (mask & Inotify.IN_ATTRIB) {
-          console.log(type + 'metadata changed ');
-      } else if (mask & Inotify.IN_CREATE) {
-          console.log(type + 'created');
-      } else if (mask & Inotify.IN_DELETE) {
-          console.log(type + 'deleted');
-      } else if (mask & Inotify.IN_DELETE_SELF) {
-          console.log(type + 'watched deleted ');
-      } else if (mask & Inotify.IN_MOVE_SELF) {
-          console.log(type + 'watched moved');
-      } else if (mask & Inotify.IN_IGNORED) {
-          console.log(type + 'watch was removed');
-      } else if (mask & Inotify.IN_MOVED_FROM) {
-          data = event;
-          data.type = type;
-      } else if (mask & Inotify.IN_MOVED_TO) {
-          if ( Object.keys(data).length &&
-              data.cookie === event.cookie) {
-              console.log(type + ' moved to ' + data.type);
-              data = {};
-          }
-      }
+    console.log(event.name);
   };
 
   dirs.forEach((dir) => {
     const watch = {
       path:      videoFolderPath + dir,
-      watch_for: Inotify.IN_ALL_EVENTS,
+      watch_for: Inotify.IN_CLOSE_WRITE,
       callback:  watchCallback
   };
   });
